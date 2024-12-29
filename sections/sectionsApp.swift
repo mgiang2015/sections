caaa4becb366b486edd409f86d60e61fa81339d5
mgiang2015 <mgiang2015@gmail.com>
2024-12-29 10:40:47 +0800

    Initial Commit

---
+++ sections/sectionsApp.swift
@@ -0,0 +1,32 @@
+//
+//  sectionsApp.swift
+//  sections
+//
+//  Created by Minh Giang Le on 29/12/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct sectionsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ sectionsTests/sectionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  sectionsTests.swift
+//  sectionsTests
+//
+//  Created by Minh Giang Le on 29/12/24.
+//
+
+import Testing
+@testable import sections
+
+struct sectionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
